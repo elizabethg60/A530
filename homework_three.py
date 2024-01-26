7c5403f5864304971b2f9e74f7f97dc5562e9d04
elizabethg60 <Eli.gonzalez1124@gmail.com>
2024-01-26 11:15:24 -0500

    hw3 done

--- homework_three.py
+++ homework_three.py
@@ -5,7 +5,7 @@ from scipy import integrate
 import scipy.special as sc
 import numpy as np
 
-## 4. Emergent Intensity Integrator
+# 4. Emergent Intensity Integrator
 
 """
 (a) Illustrate your answer to Problem 2 (from last week) by numerically integrating the
@@ -32,21 +32,22 @@ output_linear = a0*a0_coeff+a1*a1_coeff
 a2_coeff = integrate.simpson(intensity_integral(2, x_array), x_array)
 output_quadratic = a0*a0_coeff+a1*a1_coeff+a2*a2_coeff
 output_quadratic_zero = a0*a0_coeff+a1*a1_coeff+0*a2_coeff
+source_quadratic = a0*a0_coeff+a1*a1_coeff+a2
 #plot
-plt.plot(a2, [output_linear]*len(a2), linewidth = 2, color = 'r')
 plt.plot(a2, output_quadratic, linewidth = 2, color = 'k')
-plt.scatter(a2, [output_linear]*len(a2), s = 15, color = 'r', label = "Linear")
-plt.scatter(a2, output_quadratic, s = 15, color = 'k', label = "Quadratic")
-plt.scatter(0, output_quadratic_zero, s = 15, color = 'k')
+plt.plot(a2, source_quadratic, linewidth = 2, color = 'b')
+plt.scatter(a2, output_quadratic, s = 15, color = 'k', label = "EB, Quadratic")
+plt.scatter(a2, source_quadratic, s = 15, color = 'b', label = "Source, Quadratic")
+plt.scatter(0, output_linear, s = 80, marker = "*", color = 'r', label = "Linear", zorder=5)
 plt.xlabel("$a_{2}$", fontsize = 12)
-plt.ylabel("emergent intensity [erg $cm^{-2}$ $s^{-1}$ $st^{-1}$ $Hz^{-1}$]", fontsize = 12)
+plt.ylabel("[erg $cm^{-2}$ $s^{-1}$ $st^{-1}$ $Hz^{-1}$]", fontsize = 12)
 plt.xticks(fontsize = 12) 
 plt.yticks(fontsize = 12)
 plt.legend()
 plt.savefig("Figures/hw_three_figures/sample_4a.png", bbox_inches='tight')
 plt.show()
 
-#confirm: negative values of a2 results in EB overestimating whereas positive values of a2 results in EB underestimating and at a2 = 0 perfect match
+#confirm: negative values of a2 results in EB underestimate whereas positive values of a2 results in EB overestimate and at a2 = 0 perfect match
 
 """
 (b) Explain (and perhaps show) how you determined that your sampling and limits of
@@ -156,53 +157,76 @@ print("E(n = 1) = {}".format(sc.expn(1, x)))
 b) Numerically integrate the area under the first three exponential functions (that is,
 numerically integrate En(x)dx for n = 1, 2, 3). How close can you get to the analytic result? What limits your precision?
 """
-#precision for linear and quadratic functions with optimized integral parameters (above)
-truth_linear = a0+a1
-truth_quadratic = a0+a1+2*a2
-output_quadratic = a0*a0_coeff+a1*a1_coeff+a2*a2_coeff
-precision_linear = np.abs(truth_linear - output_linear)/truth_linear
-precision_quadratic = np.abs(truth_quadratic - output_quadratic)/truth_quadratic
 
-print("truth: {} vs calculation: {} gives precision of {}".format(truth_linear, output_linear, precision_linear))
-#truth: 2 vs calculation: 2.0 gives precision of 0.0
-print("truth: {} vs calculation: {} gives precision of {}".format(truth_quadratic, output_quadratic, precision_quadratic))
-#truth: 6 vs calculation: 5.999999999999998 gives precision of 2.9605947323337506e-16
+#analytic results (derived via mathematica - shared in report)
+n = [1,2,3]
+truth1 = 1/n[0]
+truth2 = 1/n[1]
+truth3 = 1/n[2]
+#numerically integrate 
+x_min = 10**(-16) 
+x_max = 50
+step = 10**(-5) 
+x_array = np.power(10, np.arange(np.log10(x_min), np.log10(x_max), step))
+expn1 = sc.expn(1, x_array)
+expn2 = sc.expn(2, x_array)
+expn3 = sc.expn(3, x_array)
+output1 = integrate.simpson(expn1, x_array)
+output2 = integrate.simpson(expn2, x_array)
+output3 = integrate.simpson(expn3, x_array)
+#precision for n = 1,2,3 with optimized integral parameters
+precision1 = np.abs(truth1 - output1)/truth1
+precision2 = np.abs(truth2 - output2)/truth2
+precision3 = np.abs(truth3 - output3)/truth3
+
+print("truth: {} vs calculation: {} gives precision of {}".format(truth1, output1, precision1))
+#truth: 1.0 vs calculation: 0.9999999999999962 gives precision of 3.774758283725532e-15
+print("truth: {} vs calculation: {} gives precision of {}".format(truth2, output2, precision2))
+#truth: 0.5 vs calculation: 0.4999999999999999 gives precision of 2.220446049250313e-16
+print("truth: {} vs calculation: {} gives precision of {}".format(truth3, output3, precision3))
+#truth: 0.3333333333333333 vs calculation: 0.33333333333333337 gives precision of 1.6653345369377348e-16
+
+"""
+c) Explain how you determined that your sampling and upper limit in part (b) are appro-
+priate and how you determined the precision of your integration. For a box or trapezoid
+integrator, you may need to experiment with appropriate samplings and limits to achieve
+an precise result. What’s the best precision you can get in a reasonable computation time
+(less than a second)? What upper limit and spacing of points gives you that precision?
+How few points can you get away with, practically speaking? (If you use a black-box
+integrator, describe how it does these things and how you know it’s working.)
+"""
+
+#given n = 1 had the worse precision with the parameters above, i will optimize parameters using n =1 
 
 #relative prescision convergence
 x_min_arr = np.logspace(-20, -10, 20)
 x_max_arr = np.logspace(1, 2, 20)
 step_arr = np.logspace(-6, -5, 20)
  
-x_min_result_quadratic = []
-x_max_result_quadratic = []
-step_result_quadratic = []
+x_min_result = []
+x_max_result = []
+step_result = []
 for i in x_min_arr:
-    x_array = np.arange(i, x_max, step)
-    a0_coeff = integrate.simpson(intensity_integral(0, x_array), x_array)
-    a1_coeff = integrate.simpson(intensity_integral(1, x_array), x_array)
-    a2_coeff = integrate.simpson(intensity_integral(2, x_array), x_array)
-    output_quadratic = a0*a0_coeff+a1*a1_coeff+a2*a2_coeff
-    precision_quadratic = np.abs(truth_quadratic - output_quadratic)/truth_quadratic
-    x_min_result_quadratic.append(precision_quadratic)
+    x_array = np.power(10, np.arange(np.log10(i), np.log10(x_max), step))
+    expn1 = sc.expn(1, x_array)
+    output1 = integrate.simpson(expn1, x_array)
+    precision1 = np.abs(truth1 - output1)/truth1
+    x_min_result.append(precision1)
 for i in x_max_arr:
-    x_array = np.arange(x_min, i, step)
-    a0_coeff = integrate.simpson(intensity_integral(0, x_array), x_array)
-    a1_coeff = integrate.simpson(intensity_integral(1, x_array), x_array)
-    a2_coeff = integrate.simpson(intensity_integral(2, x_array), x_array)
-    output_quadratic = a0*a0_coeff+a1*a1_coeff+a2*a2_coeff
-    precision_quadratic = np.abs(truth_quadratic - output_quadratic)/truth_quadratic
-    x_max_result_quadratic.append(precision_quadratic)
+    x_array = np.power(10, np.arange(np.log10(x_min), np.log10(i), step))
+    expn1 = sc.expn(1, x_array)
+    output1 = integrate.simpson(expn1, x_array)
+    precision1 = np.abs(truth1 - output1)/truth1
+    x_max_result.append(precision1)
 for i in step_arr:
-    x_array = np.arange(x_min, x_max, i)
-    a0_coeff = integrate.simpson(intensity_integral(0, x_array), x_array)
-    a1_coeff = integrate.simpson(intensity_integral(1, x_array), x_array)
-    a2_coeff = integrate.simpson(intensity_integral(2, x_array), x_array)
-    output_quadratic = a0*a0_coeff+a1*a1_coeff+a2*a2_coeff
-    precision_quadratic = np.abs(truth_quadratic - output_quadratic)/truth_quadratic
-    step_result_quadratic.append(precision_quadratic)
+    x_array = np.power(10, np.arange(np.log10(x_min), np.log10(x_max), i))
+    expn1 = sc.expn(1, x_array)
+    output1 = integrate.simpson(expn1, x_array)
+    precision1 = np.abs(truth1 - output1)/truth1
+    step_result.append(precision1)
 
-plt.plot(x_min_arr, x_min_result_quadratic, linewidth = 2, color = 'k')
-plt.scatter(x_min_arr, x_min_result_quadratic, s = 15, color = 'k')
+plt.plot(x_min_arr, x_min_result, linewidth = 2, color = 'k')
+plt.scatter(x_min_arr, x_min_result, s = 15, color = 'k')
 plt.xlabel("log min bound", fontsize = 12)
 plt.ylabel("log relative precision", fontsize = 12)
 plt.xticks(fontsize = 12) 
@@ -210,11 +234,11 @@ plt.yticks(fontsize = 12)
 plt.yscale("log")
 plt.xscale("log")
 plt.legend()
-plt.savefig("Figures/hw_three_figures/sample_4i.png", bbox_inches='tight')
+plt.savefig("Figures/hw_three_figures/sample_5bi.png", bbox_inches='tight')
 plt.show()
 
-plt.plot(x_max_arr, x_max_result_quadratic, linewidth = 2, color = 'k')
-plt.scatter(x_max_arr, x_max_result_quadratic, s = 15, color = 'k')
+plt.plot(x_max_arr, x_max_result, linewidth = 2, color = 'k')
+plt.scatter(x_max_arr, x_max_result, s = 15, color = 'k')
 plt.xlabel("log max bound", fontsize = 12)
 plt.ylabel("log relative precision", fontsize = 12)
 plt.xticks(fontsize = 12) 
@@ -222,11 +246,11 @@ plt.yticks(fontsize = 12)
 plt.yscale("log")
 plt.xscale("log")
 plt.legend()
-plt.savefig("Figures/hw_three_figures/sample_4bii.png", bbox_inches='tight')
+plt.savefig("Figures/hw_three_figures/sample_5bii.png", bbox_inches='tight')
 plt.show()
 
-plt.plot(step_arr, step_result_quadratic, linewidth = 2, color = 'k')
-plt.scatter(step_arr, step_result_quadratic, s = 15, color = 'k')
+plt.plot(step_arr, step_result, linewidth = 2, color = 'k')
+plt.scatter(step_arr, step_result, s = 15, color = 'k')
 plt.xlabel("log step width", fontsize = 12)
 plt.ylabel("log precision", fontsize = 12)
 plt.xticks(fontsize = 12) 
@@ -234,92 +258,28 @@ plt.yticks(fontsize = 12)
 plt.yscale("log")
 plt.xscale("log")
 plt.legend()
-plt.savefig("Figures/hw_three_figures/sample_4biii.png", bbox_inches='tight')
+plt.savefig("Figures/hw_three_figures/sample_5biii.png", bbox_inches='tight')
 plt.show()
 
+#numerically integrate 
+x_min = 10**(-20) 
+x_max = 50
+step = 10**(-5) 
+x_array = np.power(10, np.arange(np.log10(x_min), np.log10(x_max), step))
+expn1 = sc.expn(1, x_array)
+expn2 = sc.expn(2, x_array)
+expn3 = sc.expn(3, x_array)
+output1 = integrate.simpson(expn1, x_array)
+output2 = integrate.simpson(expn2, x_array)
+output3 = integrate.simpson(expn3, x_array)
+#precision for n = 1,2,3 with optimized integral parameters
+precision1 = np.abs(truth1 - output1)/truth1
+precision2 = np.abs(truth2 - output2)/truth2
+precision3 = np.abs(truth3 - output3)/truth3
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # #using scipy.special.expn to get analytic result for n = 1,2,3 for x = 10**(-10) (which is lower bound of integral)
-# # # x_min = 10**(-10)
-# # # truth_E0 = sc.expn(0, x_min)
-# # # truth_E1 = sc.expn(1, x_min)
-# # # truth_E2 = sc.expn(2, x_min)
-
-
-
-
-
-
-
-
-# # # x_min_arr = np.logspace(-12, -5, 50)
-# # # x_max_arr = np.logspace(2, 3, 20)
-# # # step_arr = np.logspace(-6, -5, 20)
-
-# # # x_min_result = []
-# # # x_max_result = []
-# # # step_result = []
-
-# # # x_min = 10**(-4)
-# # # x_max = 100
-# # # step = 0.00001
-
-# # # # for i in x_min_arr:
-# # # #     output = integrate_special_exp(0, i, x_max, step)
-# # # #     precision = np.abs(truth_E0 - output)/truth_E0
-# # # #     x_min_result.append(precision)
-# # # for i in x_max_arr:
-# # #     output = integrate_special_exp(0, x_min, i, step)
-# # #     precision = np.abs(truth_E0 - output)/truth_E0
-# # #     x_max_result.append(precision)
-# # # # for i in step_arr:
-# # # #     output = integrate_special_exp(0, x_min, x_max, i)
-# # # #     precision = np.abs(truth_E0 - output)/truth_E0
-# # # #     step_result.append(precision)
-
-# # # # plt.plot(x_min_arr, x_min_result, linewidth = 1)
-# # # # plt.xticks(fontsize = 12) 
-# # # # plt.yticks(fontsize = 12) 
-# # # # plt.yscale("log")
-# # # # plt.xscale("log")
-# # # # plt.show()
-
-# # # plt.plot(x_max_arr, x_max_result, linewidth = 1)
-# # # plt.xticks(fontsize = 12) 
-# # # plt.yticks(fontsize = 12) 
-# # # plt.yscale("log")
-# # # plt.xscale("log")
-# # # plt.show()
-
-# # # # plt.plot(step_arr, step_result, linewidth = 1)
-# # # # plt.xticks(fontsize = 12) 
-# # # # plt.yticks(fontsize = 12) 
-# # # # plt.yscale("log")
-# # # # plt.xscale("log")
-# # # # plt.show()
-
-
-
-
-# # # # #print(integrate_special_exp(0, x_min, 100000, 0.1))
-
-
-
-
-
-
-
-
-
+print("truth: {} vs calculation: {} gives precision of {}".format(truth1, output1, precision1))
+#truth: 1.0 vs calculation: 1.0 gives precision of 0.0
+print("truth: {} vs calculation: {} gives precision of {}".format(truth2, output2, precision2))
+#truth: 0.5 vs calculation: 0.5 gives precision of 0.0
+print("truth: {} vs calculation: {} gives precision of {}".format(truth3, output3, precision3))
+#truth: 0.3333333333333333 vs calculation: 0.33333333333333326 gives precision of 1.6653345369377348e-16
