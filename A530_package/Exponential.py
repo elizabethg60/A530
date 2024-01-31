c060eb4887860219dcf7b01abe3adddc688c67ce
elizabethg60 <Eli.gonzalez1124@gmail.com>
2024-01-31 16:42:01 -0500

    hw4 done

--- A530_package/Exponential.py
+++ A530_package/Exponential.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.special as sc
+from scipy import integrate
 from A530_package import box_integrator
 
 def special_exp_integral(n, t):
@@ -10,3 +12,17 @@ def integrate_special_exp(n, x_min, x_max, step):
     x_array = np.arange(x_min, x_max, step)
     integral = special_exp_integral(n, x_array)
     return (x_min**(n-1))*box_integrator(x_array, integral)
+
+def quadratic_source(tau_array, a0, a1, a2):
+# returns a quadratic source function for given a_n values and array of optical depth
+    return a0 + a1*tau_array + a2*tau_array**2
+
+def linear_source(tau_array, a0, a1, a2):
+# returns a linear source function for given a_n values and array of optical depth
+    return a0 + a1*tau_array
+
+def eddington_flux(source_func, tau_array, a0, a1, a2):
+# computes the eddington flux 
+    E_2 = sc.expn(2, tau_array)
+    S_v = source_func(tau_array, a0, a1, a2)
+    return 0.5*integrate.simpson(E_2*S_v, tau_array)
---
+++ homework_four.py
@@ -0,0 +1,30 @@
+from A530_package import quadratic_source, linear_source, eddington_flux
+from scipy import integrate
+import scipy.special as sc
+import numpy as np
+
+"""
+7. Emergent Eddington Flux Hv(0)
+Use your work from Problem 5 to write a function that calculates Hv(0) given an array of
+optical depths τv using an exponential integral. Choose a quadratic form for Sv(τv) and compute Hv (0) for it.
+Confirm that this works as you expect it to for a linear source function.
+"""
+
+#all related functions are found under A530_package/Exponential.py
+
+#hardwire values for a_n coefficients
+a0 = a1 = 1
+a2 = 2
+
+#set array of optical depths 
+x_min = 10**(-16) 
+x_max = 50
+step = 10**(-5) 
+tau_array = np.power(10, np.arange(np.log10(x_min), np.log10(x_max), step))
+
+print("eddington flux for quadratic source function: {}".format(eddington_flux(quadratic_source, tau_array, a0, a1, a2)))
+#eddington flux for quadratic source function: 0.9166666666666665
+print("eddington flux for linear source function: {}".format(eddington_flux(linear_source, tau_array, a0, a1, a2)))
+#eddington flux for linear source function: 0.41666666666666663
+
+#smaller eddington flux for linear source function as expected 
