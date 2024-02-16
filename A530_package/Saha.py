746c3e0a613998a4031ecea36418e6b441e36ac5
elizabethg60 <Eli.gonzalez1124@gmail.com>
2024-02-16 10:29:13 -0500

    hw 6 done

---
+++ A530_package/Saha.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
+
+def partition(element, temperature, data, temp_arr):
+# returns partition function given an arbitrary element and temperature 
+
+    #HII case
+    if element == "HII":
+        return 0.0
+    #H- case
+    if element == "H-":
+        return -(5040/temperature)*(0.755)
+
+    #if element not found, return log partition function of 0.0
+    if len(data.loc[data[0] == element]) == 0.0:
+        return 0.0
+
+    #collect true parition functions from table
+    true_partition = np.array(data.loc[data[0] == element])[0][1:-1]
+
+    partition_arr = []
+    start = 0
+    for i in range(0, len(true_partition)):
+        if true_partition[i] != "-":
+            partition_arr.append(float(true_partition[i]))
+        if true_partition[i] == "-":
+            start = i + 1
+
+    #determine new array of temperatures for known partitions 
+    new_temp_arr = temp_arr[start:,]
+
+    #fit a cubic spline to data to get partition function at a given temp
+    cs = CubicSpline(new_temp_arr[::-1], partition_arr[::-1])
+    #plot for confirmation 
+    plt.scatter(new_temp_arr, partition_arr)
+    x_range = np.arange(min(new_temp_arr), max(new_temp_arr), 0.1)
+    plt.plot(x_range, cs(x_range), label='Cubic Spline')
+    plt.xlabel("temperature (K)")
+    plt.ylabel("log partition function")
+    plt.show()
+
+    return cs(temperature)
+
+def saha(element, temperature, data, temp_arr, ion_potential):
+# returns temperature dependent part of RHS of Saha equation given an arbitrary element and temperature
+
+    #H- case
+    if element == "H-":
+        U = 10**partition(element, temperature, data, temp_arr)
+        U_plus = 10**partition("H", temperature, data, temp_arr)
+
+    #every other case
+    else: 
+        U = 10**partition(element, temperature, data, temp_arr)
+        U_plus = 10**partition(element+"+", temperature, data, temp_arr) 
+
+    return 0.6665*(U_plus/U)*(temperature**(5/2))*10**(-(5040/temperature)*ion_potential)
--- homework_six.py
+++ homework_six.py
@@ -1,24 +1,26 @@
+import numpy as np
 import pandas as pd
+from A530_package import partition, saha
 
 """
 11. Partition Functions
 Write code to calculate the partition function for an arbitrary species at an arbitrary
-temperature. We will rarely deal with double ionization, so your code does not have to
-successfully handle every case of a doubly ionized element. Hints:
-• When you solve the Saha equation for the ionization levels of atoms, especially
-hydrogen, you will need to have values for partition functions of things like Hii, and
-H-, so be sure your partition function returns useful numbers for these.
-• H- has an ionization potential of 0.755 eV and you will recall that it has exactly
-one bound state — what do you suppose its electron configuration is?
-• What does this imply about the occupation number g0 and the partition function
-UH- (T )?
+temperature. 
 """
 
+#read in data
 partition_function = pd.read_csv('data/RepairedPartitionFunctions.txt', sep= ' ', header=None)
 
-#to do: (1) confirm with Gray's appendix and understand parition table or is it statistical weights 
-#       (2) understand problem (check Gray, Rutten is pg 30 only)
-#       (3) work on problem THEN consider hints 
+#create temp array
+theta_arr = np.arange(0.2, 2.2, step = 0.2)
+temp_arr = 5040/theta_arr
+
+#calculate the partition function for an arbitrary species at an arbitrary temperature
+element = "Tm"
+temperature = 4000
+# #returns log of the partition function
+# print("log partition function: {}".format(partition(element, temperature, partition_function, temp_arr)))
+# print("partition function: {}".format(10**(partition(element, temperature, partition_function, temp_arr))))
 
 """
 13. Saha's Φ(T)
@@ -26,9 +28,14 @@ Write a function Φ(T) that returns the temperature dependent part of the RHS of
 Saha equation for an arbitrary element.
 """
 
+#read in data 
 ionization_potential = pd.read_fwf('data/ioniz.txt', header=None)
-#Atomic number, Element, Average atomic weight, 1st, 2nd, and 3rd ionization energies in eV.  
-#to get specific element index by row number - 1 using .iloc
-#ionization_potential.loc[ionization_potential[1] == "element"]
 
-#to do: (1) solve saha equation for temperature (2) figure out LHS (3) write function and finish answering question
+#determine (first) ionization potential for given element (no second / third ionization potential at this time)
+if element == "H-":
+    element_ion_pot = 0.755
+else: 
+    element_ion_pot = float(ionization_potential.loc[ionization_potential[1] == element][3])
+
+#return temperature dependent part of RHS of Saha equation 
+print("Saha temperature dependent: {}".format(saha(element, temperature, partition_function, temp_arr, element_ion_pot)))
